b0957bd94f288a6ff8c1da01584756e1d430a7d8
alichatraei <avida.chatraee2001@yahoo.com>
2023-08-31 10:35:03 +0330

    change

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  basePath: "/ali-chatraei",
-  assetPrefix: "/ali-chatraei/",
-};
-
-module.exports = nextConfig;
